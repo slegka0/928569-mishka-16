96d00e0cdef40cbb190e5819169cb15da8c5b36b
BelyaevDmitry <beldmitry03@gmail.com>
2019-05-12 19:11:15 +0300

    Добавляет модалки

---
+++ source/js/script.js
@@ -0,0 +1,19 @@
+var modal = document.querySelector(".modal");
+var modal__submit = document.querySelector(".modal__submit");
+var order__button = document.querySelector(".button--js");
+var overlay = document.querySelector(".overlay");
+
+order__button.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  modal.classList.add("modal--show");
+  overlay.classList.add("overlay--show");
+});
+
+document.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    modal.classList.remove("modal--show");
+    overlay.classList.remove("overlay--show");
+  }
+});
+
+
