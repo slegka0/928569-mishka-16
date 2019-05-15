903626a5c41b2e4e2fc0cf1f846f848ec1cfd595
BelyaevDmitry <beldmitry03@gmail.com>
2019-05-15 18:37:10 +0300

    Подготовка к защите

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,14 @@ var sass = require("gulp-sass");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var rename = require("gulp-rename");
+var csso = require("gulp-csso");
+var gsmin = require("gulp-uglify");
+var imagemin = require("gulp-imagemin");
+var del = require("del");
+var posthtml = require("gulp-posthtml");
+var htmlmin = require("gulp-htmlmin");
+var include = require("posthtml-include")
 
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
@@ -16,22 +24,83 @@ gulp.task("css", function () {
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
 });
 
 gulp.task("server", function () {
   server.init({
-    server: "source/",
+    server: "build/",
     notify: false,
     open: true,
     cors: true,
     ui: false
   });
 
+  gulp.watch("source/js/*.js", gulp.series("js", "refresh"));
   gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
-  gulp.watch("source/*.html").on("change", server.reload);
+  gulp.watch("source/*.html", gulp.series("html", "refresh"));
 });
 
-gulp.task("start", gulp.series("css", "server"));
+gulp.task("refresh", function (done) {
+  server.reload();
+  done();
+});
+
+gulp.task("cssmin", function () {
+  return gulp.src("source/css/*.css")
+  .pipe(csso())
+  .pipe(rename(function (path) {
+    path.basename += ".min";
+  }))
+  .pipe(gulp.dest("build/css"))
+});
+
+gulp.task("copy", function () {
+  return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/**",
+    "source/*.ico"
+    ], {
+    base: "source"
+  })
+  .pipe(gulp.dest("build"));
+});
+
+gulp.task("images", function () {
+  return gulp.src("source/img/*.{png, jpg, svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("source/img"))
+});
+
+gulp.task("html", function () {
+  return gulp.src("source/*.html")
+    .pipe(posthtml([
+      include()
+    ]))
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("js", function () {
+  return gulp.src("source/js/*.js")
+    .pipe(gsmin())
+    .pipe(rename(function (path) {
+      path.basename += ".min";
+    }))
+    .pipe(gulp.dest("build/js"));
+});
+
+gulp.task("clean", function () {
+  return del("build");
+});
+
+gulp.task("build", gulp.series("clean", "copy", "js", "css", "html", "cssmin"));
+gulp.task("start", gulp.series("build", "server"));
--- source/js/script.js
+++ source/js/script.js
@@ -1,13 +1,17 @@
 var modal = document.querySelector(".modal");
 var modal__submit = document.querySelector(".modal__submit");
-var order__button = document.querySelector(".button--js");
+var order__button = document.querySelectorAll(".button--js");
 var overlay = document.querySelector(".overlay");
 
-order__button.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  modal.classList.add("modal--show");
-  overlay.classList.add("overlay--show");
-});
+if(order__button.length) {
+  for (var i = 0; i < order__button.length; i++) {
+    order__button[i].addEventListener("click", function (evt) {
+      evt.preventDefault();
+      modal.classList.add("modal--show");
+      overlay.classList.add("overlay--show");
+    });
+  }
+}
 
 document.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
