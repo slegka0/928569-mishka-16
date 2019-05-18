438fe052d84b076dc0416fcf2839138446171470
BelyaevDmitry <beldmitry03@gmail.com>
2019-05-18 16:37:22 +0300

    Правка базовых критериев + реализация меню через js

--- source/js/script.js
+++ source/js/script.js
@@ -2,6 +2,10 @@ var modal = document.querySelector(".modal");
 var modal__submit = document.querySelector(".modal__submit");
 var order__button = document.querySelectorAll(".button--js");
 var overlay = document.querySelector(".overlay");
+var header__nav = document.querySelector(".site-navigation");
+var header__button = document.querySelector(".site-navigation__button");
+
+header__nav.classList.remove('site-navigation--nojs');
 
 if(order__button.length) {
   for (var i = 0; i < order__button.length; i++) {
@@ -17,5 +21,15 @@ document.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
     modal.classList.remove("modal--show");
     overlay.classList.remove("overlay--show");
+  }
+});
+
+header__button.addEventListener('click', function() {
+  if (header__nav.classList.contains('site-navigation--closed')) {
+    header__nav.classList.remove('site-navigation--closed');
+    header__nav.classList.add('site-navigation--opened');
+  } else {
+    header__nav.classList.add('site-navigation--closed');
+    header__nav.classList.remove('site-navigation--opened');
   }
 });
